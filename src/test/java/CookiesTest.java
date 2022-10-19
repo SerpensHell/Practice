ac78c57161628cb956e34e8668d50ec893fc7a7a
ПВ-41 Забелина Екатерина <katyazabelina1949@gmail.com>
2022-10-19 16:00:30 +0300

    practice 5 point complete

--- src/test/java/CookiesTest.java
+++ src/test/java/CookiesTest.java
@@ -3,7 +3,10 @@ import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
+
+import java.util.Set;
 
 public class CookiesTest {
     protected static WebDriver driver;
@@ -31,5 +34,33 @@ public class CookiesTest {
     public void cookiesTest(){
         driver.get("https://www.dns-shop.ru/");
         logger.info("Открыта страница DNS - " + "https://www.dns-shop.ru/");
+        // Создание куки Cookie 1
+        driver.manage().addCookie(new Cookie("Cookie 1", "This Is Cookie 1"));
+        // Вывод информации по куки Cookie 1
+        logger.info("Куки, которое добавили мы");
+        Cookie cookie1  = driver.manage().getCookieNamed("Cookie 1");
+        logger.info(String.format("Domain: %s", cookie1.getDomain()));
+        logger.info(String.format("Expiry: %s",cookie1.getExpiry()));
+        logger.info(String.format("Name: %s",cookie1.getName()));
+        logger.info(String.format("Path: %s",cookie1.getPath()));
+        logger.info(String.format("Value: %s",cookie1.getValue()));
+        logger.info("--------------------------------------");
+        // Вывод информации по кукам dns-shop.ru
+        logger.info("Куки, которое добавил DNS");
+        Set<Cookie> cookies = driver.manage().getCookies();
+        for(Cookie cookie : cookies) {
+            logger.info(String.format("Domain: %s", cookie.getDomain()));
+            logger.info(String.format("Expiry: %s", cookie.getExpiry()));
+            logger.info(String.format("Name: %s", cookie.getName()));
+            logger.info(String.format("Path: %s", cookie.getPath()));
+            logger.info(String.format("Value: %s", cookie.getValue()));
+            logger.info("--------------------------------------");
+        }
+        // Добавление задержки Thread.sleep, чтобы увидеть результат
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
